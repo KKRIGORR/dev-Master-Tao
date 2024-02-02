3c5209c12a8f415903c30f65bfccb12791bda7a3
KKRIGORR <thekrigor@gmail.com>
2024-02-02 15:17:28 +0200

    advantages

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -15,6 +15,36 @@ $(document).ready(function(){
         ]
   });
 
+  
+  mobileOnlySlider("#slider-advantages", true, false, 1024);
+
+  function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+    
+    var slider = $($slidername);
+    var settings = {
+      mobileFirst: true,
+      dots: $dots,
+      arrows: $arrows,
+      responsive: [
+        {
+          breakpoint: $breakpoint,
+          settings: "unslick"
+        }
+      ]
+    };
+  
+    slider.slick(settings);
+  
+    $(window).on("resize", function () {
+      if ($(window).width() > $breakpoint) {
+        return;
+      }
+      if (!slider.hasClass("slick-initialized")) {
+        return slider.slick(settings);
+      }
+    });
+  } // Mobile Only Slider
+  
   const burger = document.querySelector('.btn-burger');
   const navigation = document.querySelector('.header__navigation');
 
